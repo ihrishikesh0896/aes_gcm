4eef023b5e29711baf7112c89c7d67804fb327fd
procrastinator <ihrishikeshnate@gmail.com>
2024-01-29 15:11:32 +0530

    package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aes_encryption',
+    name='aesgcm',
     version='0.1.1',
     packages=find_packages(),
     description='A simple AES encryption library',
